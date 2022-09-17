a799bc59fff6ab63a72343ba2fd4050486bf0cb6
DuderCoding <dhilen.p@yahoo.com>
2022-09-17 16:39:10 +0100

    Removed redundant code checking for zero, added it to other if statement

--- main.py
+++ main.py
@@ -3,20 +3,18 @@ import random
 def game_start():
     incorrect_input = True
     while(incorrect_input is True):
-        upper_bound = input("Please enter an upper range for the guessing game: ").lstrip("0")
+        upper_bound = input("Please enter an upper range for the guessing game: ")
         incorrect_input = check_user_input(upper_bound)
         print()
+    upper_bound = upper_bound.lstrip("0")
     print("Let the games begin...\nI am thinking of a number between 1 and {}. Can you guess what it is? ".format(upper_bound))
     computers_number = random.randint(1, int(upper_bound))
     print(computers_number)
 
 def check_user_input(user_input):
-    if(user_input == ""):
-        print("Incorrect input. Please choose a number greater than 0")
-        return True
     x = re.findall("^[+]?([1-9]&|[0-9])+$",user_input)
-    if len(x) == 0:
-        print("Incorrect input. Please choose a number not: {}".format(user_input))
+    if len(x) == 0 or user_input == "0":
+        print("Incorrect input. Please choose a number greater than 0, not: {}".format(user_input))
         return True
     else:
         return False
