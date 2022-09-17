82898b4d4e4cb2e8201768d08084903675802d1a
DuderCoding <dhilen.p@yahoo.com>
2022-09-17 13:16:01 +0100

    Added RegEx to check the user inputs a number

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import re
 
 def game_start():
     upper_bound = input("Please enter an upper range for the guessing game: ")
-    print(upper_bound)
+    x = re.findall("^[+]?[0-9]+$",upper_bound)
+
+    if len(x) == 0:
+        print("Incorrect input")
+    else:
+        print("Let the games begin")
 
 if __name__ == '__main__':
     game_start()
