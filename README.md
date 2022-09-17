5d1fa631ff18ff6b7fb1be336fb07cbdddc44332
DuderCoding <dhilen.p@yahoo.com>
2022-09-17 11:56:02 +0100

    Created starting function and takes user's upper bound for the random number generation

---
+++ README.md
@@ -0,0 +1,4 @@
+
+A simple python project to get back into python programming.
+
+A random number guessing game, purely console based.
---
+++ main.py
@@ -0,0 +1,8 @@
+
+def game_start():
+    upper_bound = input("Please enter an upper range for the guessing game: ")
+    print(upper_bound)
+
+if __name__ == '__main__':
+    game_start()
+
