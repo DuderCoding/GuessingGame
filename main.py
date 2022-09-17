b0156624224e2680979301f44878ad9c2afde76c
DuderCoding <dhilen.p@yahoo.com>
2022-09-17 15:32:01 +0100

    Added a loop to keep checking input when user does it incorrectly, added a function for checking user input is correct

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import re
 
 def game_start():
-    upper_bound = input("Please enter an upper range for the guessing game: ")
-    x = re.findall("^[+]?[0-9]+$",upper_bound)
+    incorrect_input = True
+    while(incorrect_input is True):
+        upper_bound = input("Please enter an upper range for the guessing game: ")
+        incorrect_input = check_user_input(upper_bound)
 
+def check_user_input(user_input):
+    x = re.findall("^[+]?[0-9]+$",user_input)
     if len(x) == 0:
-        print("Incorrect input")
+        print("Incorrect input. Please choose a number not: {}".format(user_input))
+        print()
+        return True
     else:
         print("Let the games begin")
+        return False
 
 if __name__ == '__main__':
     game_start()
