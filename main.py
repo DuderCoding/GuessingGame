28f7e2521827d983bb38e9f698d9e358d576dec3
DuderCoding <dhilen.p@yahoo.com>
2022-09-18 20:48:58 +0100

    Added function for taking the user guesses and comparing them with the random number

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import re
 import random
+
 def game_start():
     incorrect_input = True
     while(incorrect_input is True):
@@ -9,8 +10,11 @@ def game_start():
     upper_bound = upper_bound.lstrip("0")
     print("Let the games begin...\nI am thinking of a number between 1 and {}. Can you guess what it is? ".format(upper_bound))
     computers_number = random.randint(1, int(upper_bound))
-    hints(computers_number)
+    computer_hints = hints(computers_number)
+
     print(computers_number)
+
+    user_guesses(computers_number,computer_hints)
 
 def check_user_input(user_input):
     x = re.findall("^[+]?([1-9]&|[0-9])+$",user_input)
@@ -33,7 +37,28 @@ def hints(number):
         hints_array.append("Multiple of 4")
     if(number%5 == 0):
         hints_array.append("Multiple of 5")
-    print(hints_array)
+    return hints_array
+
+def user_guesses(computers_number,computer_hints):
+    current_round = 1
+    number_of_fixed_hints = len(computer_hints)
+    continue_game = True
+    incorrect_input = True
+    player_guess = 0
+    while(continue_game is True):
+        print("Round {}".format(current_round))
+        while (incorrect_input is True):
+            player_guess = input("Enter a guess: ")
+            incorrect_input = check_user_input(player_guess)
+            print()
+        incorrect_input = True
+
+        if(int(player_guess) == computers_number):
+            continue_game = False
+            print("You got it right. Well done, the game has finished.")
+        else:
+            current_round = current_round + 1
+            print("Nope. Try again!")
 
 if __name__ == '__main__':
     game_start()
